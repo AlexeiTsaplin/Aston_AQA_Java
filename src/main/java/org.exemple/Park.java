5d6176fa15881252c5b4dc01a992ca0d482b9ef3
AlexeiTsaplin <m0zg0v1k@yandex.ru>
2024-07-31 11:46:29 +0300

    Lesson_5

--- src/main/java/org.exemple/Park.java
+++ src/main/java/org.exemple/Park.java
@@ -1,11 +1,14 @@
 package org.exemple;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class Park {
     private String park_name;
-    private Attraction Attraction;
+    private List<Attraction> attractions;
 
     @Data
     public class Attraction {
@@ -13,5 +16,16 @@ public class Park {
         private String working_hours;
         private int price;
     }
+
+    public Park(String name) {
+        this.park_name = park_name;
+        this.attractions = new ArrayList<>();
+    }
+
+    public void addAttraction(String att_name, String working_hours, int price) {
+        Attraction newAttraction = new Attraction();
+        attractions.add(newAttraction);
+    }
 }
+
 
